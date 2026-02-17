174a5919226a9d5018f91ee4cdaf9cd956829b58
meenaaashish96 <meenaaashish96@gmail.com>
2026-02-17 18:47:52 +0530

    factorial number

--- loop/05_solution.py
+++ loop/05_solution.py
@@ -1,1 +1,14 @@
 #find first not repeated char
+string = "aashish"
+for char in string:
+    if string.count(char) == 1:
+        print("First not repeated char is:", char)
+        break;
+    # prechar = char
+    # charcount = 0;
+    # for newchar in string:
+    #     if newchar == prechar:
+    #         charcount +=1;
+    # if(charcount == 1):
+    #     print("First not repeated char is: ",prechar)
+    
---
+++ loop/06_solution.py
@@ -0,0 +1,12 @@
+#find factorial of a number using while loop
+n = 6
+factorial = 1
+# for i in range(n,1,-1):
+#     factorial *= i;
+# print(factorial);
+
+while n > 0:
+    factorial *= n
+    n = n-1
+
+print(f"Factorial of given number is:",factorial)
